6c8b6eb356e4af36e4e3471a62bfe8dcd3f3dfbf
ankit9aur <ankit9aur@gmail.com>
2023-06-30 14:24:29 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async() =>{
     try{
-        const conn =await mongoose.connect(process.env.MONGO_URI,{
+        const conn =await mongoose.connect("mongodb+srv://ankit:ankit@cluster0.oghigtf.mongodb.net/",{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const generateToken = (id) =>{
-    return jwt.sign({id},JWT_SECRET,{
+    return jwt.sign({id},"ankit",{
         expiresIn: "30d"
     });
 };
